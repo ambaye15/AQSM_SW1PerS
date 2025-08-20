d219dd8d50d1e733f620eb1c14be436ef3c8ab59
Austin MBaye <136518662+ambaye15@users.noreply.github.com>
2025-08-20 10:28:03 -0400

    Update accelerometer_SW1PerS.py

--- scripts/accelerometer_SW1PerS.py
+++ scripts/accelerometer_SW1PerS.py
@@ -16,6 +16,19 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 from AQSM_SW1PerS.SW1PerS import *
 from AQSM_SW1PerS.utils.accelerometer import *
+import signal
+
+class Timeout:
+    def __init__(self, seconds=60, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 
 def extract_processed_window(X, meta_entry, expected_length=380, sigma=3):
     """Extract smoothed and interpolated window from raw accelerometer data."""
@@ -40,22 +53,22 @@ def SW1PerS_alt(accelerometer_X, meta_data, method = 'PS1'):
     for meta in meta_data:
 
         try:
-            
-            accelerometer_window = extract_processed_window(accelerometer_X, meta)
-
-            timestamps_sec = (accelerometer_window[:, 0] - accelerometer_window[0, 0]) / 1000.0
-            ax, ay, az = accelerometer_window[:, 1], accelerometer_window[:, 2], accelerometer_window[:, 3]
-            fx, fy, fz = CubicSpline(timestamps_sec, ax), CubicSpline(timestamps_sec, ay), CubicSpline(timestamps_sec, az)
-            spline_funcs = [fx, fy, fz]
-            sampling_rate = estimate_sampling_rate(accelerometer_window[:, 0])
-
-            num_points = int(4 * sampling_rate)
-
-            scoring_pipeline = SW1PerS(start_time = 0, end_time = 4, num_points = num_points, method = method, d = d, prime_coeff = prime_coeff)
-
-            score = scoring_pipeline.compute_score(spline_funcs)
-
-            periodicity_scores.append(score)
+            with Timeout(seconds=60):
+                accelerometer_window = extract_processed_window(accelerometer_X, meta)
+    
+                timestamps_sec = (accelerometer_window[:, 0] - accelerometer_window[0, 0]) / 1000.0
+                ax, ay, az = accelerometer_window[:, 1], accelerometer_window[:, 2], accelerometer_window[:, 3]
+                fx, fy, fz = CubicSpline(timestamps_sec, ax), CubicSpline(timestamps_sec, ay), CubicSpline(timestamps_sec, az)
+                spline_funcs = [fx, fy, fz]
+                sampling_rate = estimate_sampling_rate(accelerometer_window[:, 0])
+    
+                num_points = int(4 * sampling_rate)
+    
+                scoring_pipeline = SW1PerS(start_time = 0, end_time = 4, num_points = num_points, method = method, d = d, prime_coeff = prime_coeff)
+    
+                score = scoring_pipeline.compute_score(spline_funcs)
+    
+                periodicity_scores.append(score)
             
         except Exception as e:
             
